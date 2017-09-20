a004b2c05b2d6508aee5f868b287453decef69b7
Aniello <andimarc@microsoft.com>
2017-09-20 14:28:11 -0700

    table updates (in progress)

--- AzureFunctions.AngularClient/src/app/app.module.ts
+++ AzureFunctions.AngularClient/src/app/app.module.ts
@@ -44,7 +44,7 @@ export class AppModule {
     declarations: [
       AppComponent,
       ErrorListComponent,
-      DisabledDashboardComponent,
+      DisabledDashboardComponent
     ],
     imports: [
       SharedModule.forRoot(),

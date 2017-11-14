3422e49012687d6bcb15833cf53e25132ccfd447
Aniello <andimarc@microsoft.com>
2017-11-13 17:58:49 -0800

    fixup (settings)

--- AzureFunctions.AngularClient/src/app/site/site-config/default-documents/default-documents.component.ts
+++ AzureFunctions.AngularClient/src/app/site/site-config/default-documents/default-documents.component.ts
@@ -280,15 +280,17 @@ export class DefaultDocumentsComponent implements OnChanges, OnDestroy {
     return this._translateService.instant(PortalResources.configUpdateFailureInvalidInput, { configGroupName: configGroupName });
   }
 
-  deleteItem(group: FormGroup) {
-    let groups = this.groupArray;
-    let index = groups.controls.indexOf(group);
-    if (index >= 0) {
-      if ((group as CustomFormGroup)._msExistenceState === 'original') {
-        this._deleteOriginalItem(groups, group);
-      }
-      else {
-        this._deleteAddedItem(groups, group, index);
+  deleteItem(group: FormGroup, ignore?: boolean) {
+    if (!ignore) {
+      let groups = this.groupArray;
+      let index = groups.controls.indexOf(group);
+      if (index >= 0) {
+        if ((group as CustomFormGroup)._msExistenceState === 'original') {
+          this._deleteOriginalItem(groups, group);
+        }
+        else {
+          this._deleteAddedItem(groups, group, index);
+        }
       }
     }
   }
